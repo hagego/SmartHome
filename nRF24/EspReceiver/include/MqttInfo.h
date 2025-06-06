c504c06ef7ede33b8f977aba312db7af2b8b5d18
hagego <hagen.goller@posteo.de>
2025-06-06 17:18:35 +0200

    added BME680 readout

--- nRF24/EspReceiver/include/MqttInfo.h
+++ nRF24/EspReceiver/include/MqttInfo.h
@@ -7,12 +7,27 @@
 #define MQTT_SERVER "192.168.178.27"    // IP address of the MQTT broker
 #define MQTT_PORT   1883                // port of the MQTT broker
 
-#define MQTT_CLIENT_ID "nRF24Receiver"     // client ID of the MQTT client
-#define MQTT_PREFIX    "nRF24Receiver/"    // prefix for all MQTT topics
+#define MQTT_CLIENT_ID      "nRF24Receiver"              // client ID of the MQTT client
+#define MQTT_PREFIX         "nRF24Receiver/"             // prefix for all MQTT topics related to receiver
+#define MQTT_PREFIX_SENSOR  "sensor/livingroom/"         // prefix for all MQTT topics related to sensor measurements
+#define MQTT_PREFIX_MOTION1 "motionsensor/livingroom/"   // prefix for all MQTT topics related to motion detection
 
 // MQTT publish topics
 const char* topicPublishConnected       = MQTT_PREFIX "connected";       // gets published right after connect
+const char* topicPublishIsAlive         = MQTT_PREFIX "isAlive";         // gets published right after connect
 const char* topicPublishPayloadReceived = MQTT_PREFIX "payloadReceived"; // gets published when a payload is received
+
+// MQTT motion detection publish topics
+const char* topicPublishMotion1Detected    = MQTT_PREFIX_MOTION1 "motionDetected"; // gets published when motion is detected by motion sensor 1
+const char* topicPublishMotion1Voltage     = MQTT_PREFIX_MOTION1 "battery";        // gets published when motion sensor 1 battery voltage is read
+const char* topicPublishMotion1Illuminance = MQTT_PREFIX_MOTION1 "illuminance";    // gets published when motion sensor 1 illuminance is read
+
+// MQTT sensor data publish topics
+const char* topicPublishSensorTemperature  = MQTT_PREFIX_SENSOR "temperature";   // sensor temperature data
+const char* topicPublishSensorHumidity     = MQTT_PREFIX_SENSOR "humidity";      // sensor humidity data
+const char* topicPublishSensorIaq          = MQTT_PREFIX_SENSOR "iaq";           // sensor IAQ data
+const char* topicPublishSensorIaqAccuracy  = MQTT_PREFIX_SENSOR "iaqAccuracy";   // sensor IAQ accuracy data
+const char* topicPublishSensorCo2          = MQTT_PREFIX_SENSOR "co2";           // sensor CO2 data
 
 // MQTT subscription topics
 const char* topicSubscribeSetAddress      = MQTT_PREFIX "setAddress";         // set nRF24 address 0 (max. 5 bytes/characters)()
--- nRF24/EspReceiver/src/EspReceiver.cpp
+++ nRF24/EspReceiver/src/EspReceiver.cpp
@@ -2,16 +2,35 @@
  * @file main.cpp
  */
 
- #include <Arduino.h>
- #include <ESP8266WiFi.h>
- #include <PubSubClient.h>
- #include <SPI.h>
- #include <nRF24L01.h>
- #include <RF24.h>
- 
- #include "WifiInfo.h"
- #include "MqttInfo.h"
- 
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <bsec2.h>
+
+#include "WifiInfo.h"
+#include "MqttInfo.h"
+
+
+
+/**
+ * @brief : This function checks the BSEC status, prints the respective error code. Halts in case of error
+ * @param[in] bsec  : Bsec2 class object
+ */
+void checkBsecStatus(Bsec2 bsec);
+
+/**
+ * @brief : This function is called by the BSEC library when a new output is available
+ * @param[in] input     : BME68X sensor data before processing
+ * @param[in] outputs   : Processed BSEC BSEC output data
+ * @param[in] bsec      : Instance of BSEC2 calling the callback
+ */
+void newDataCallback(const bme68xData data, const bsecOutputs outputs, Bsec2 bsec);
+
+
+
  // speed of serial interface for debug messages
  #define SERIAL_SPEED 74880
  
@@ -19,10 +38,12 @@
  const uint8_t PIN_CE  = 15; // D8 on D1 mini (SPI CS)
  const uint8_t PIN_CSN = 0;  // D3 on D1 mini (GPIO0)
  
- // global WiFi, MQTT and RF24 client objects
+ // global WiFi, MQTT, RF24 and Bsecs client objects
  WiFiClient   wifiClient;
  PubSubClient mqttClient(wifiClient);
  RF24         radio(PIN_CE, PIN_CSN);  // create an RF24 object, CE, CSN
+Bsec2         envSensor;
+
  
  // global buffer object
  char buffer[256];
@@ -30,14 +51,11 @@
  // MQTT callback function
  void mqttCallback(const char topic[], byte* payload, unsigned int length);
  
- // nRF24 addresses (5 bytes)
- const byte nRF24Address[6] = "hageg";
-
  // nRF24 addresses to listen to
   // 0: hageg: generic
   // 1: 1moti: motion sensor 1
   // 2: 2moti: motion sensor 2
- const uint8_t nRF24Addresses[][6] = {"hageg", "1moti", "2moti"};
+ uint8_t nRF24Addresses[][6] = {"hageg", "1moti", "2moti"};
  
 void setup() {
   // put your setup code here, to run once:
@@ -91,9 +109,46 @@ void setup() {
   // Once connected, publish an announcement...
   sprintf(buffer,"connected as %s",fullMqttClientName);
   mqttClient.publish(topicPublishConnected, buffer);
+  mqttClient.publish(topicPublishIsAlive, "ping");
 
   // subscribe to topics
   mqttClient.subscribe(topicSubscribeSetAddress);
+
+  /* Desired subscription list of BSEC2 outputs */
+  bsecSensor sensorList[] = {
+          BSEC_OUTPUT_STABILIZATION_STATUS,
+          BSEC_OUTPUT_RUN_IN_STATUS,
+          BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+          BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+          BSEC_OUTPUT_STATIC_IAQ,
+          BSEC_OUTPUT_CO2_EQUIVALENT,
+          BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+          BSEC_OUTPUT_GAS_PERCENTAGE,
+          BSEC_OUTPUT_COMPENSATED_GAS
+  };
+
+  /* Initialize the BME680 sensor library and interfaces */
+  Wire.begin();
+  if (!envSensor.begin(BME68X_I2C_ADDR_HIGH, Wire))
+  {
+      checkBsecStatus(envSensor);
+  }
+	
+	/*
+	 *	The default offset provided has been determined by testing the sensor in LP and ULP mode on application board 3.0
+	 *	Please update the offset value after testing this on your product 
+	 */
+	envSensor.setTemperatureOffset(TEMP_OFFSET_ULP);
+
+
+  /* Subsribe to the desired BSEC2 outputs */
+  if (!envSensor.updateSubscription(sensorList, ARRAY_LEN(sensorList), BSEC_SAMPLE_RATE_ULP))
+  {
+    checkBsecStatus(envSensor);
+  }
+
+  /* Whenever new data is available call the newDataCallback function */
+  envSensor.attachCallback(newDataCallback);
 
   // start nRF24 radio
   radio.begin();
@@ -108,7 +163,12 @@ void setup() {
   radio.startListening();            // set module as receiver
  }
  
+// Main loop
+// use simple counter to send keepalive messages to MQTT broker roughly every half hour
+u32_t keepAliveCounter = 0;
 void loop() {
+  keepAliveCounter++;
+
   // reconnect to WIFI if needed
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("WIFI disconnected");
@@ -142,7 +202,31 @@ void loop() {
     ESP.restart();
   }
 
-  if(radio.available()) {
+  // check for data from motion sensor 1
+  while(radio.available(nRF24Addresses[1])) {
+    char text[16] = {0};
+    radio.read(&text, sizeof(text));
+    Serial.print(F("nRF24 payload received from motion sensor 1: "));
+    Serial.println(text);
+
+    if(strncmp(text,"M",1)==0) {
+      Serial.println(F("Motion detected by motion sensor 1"));
+      mqttClient.publish(topicPublishMotion1Detected, "on");
+    }
+
+    if(strncmp(text,"V",1)==0 && strlen(text)>2) {
+      Serial.println(F("Battery voltage reported by motion sensor 1"));
+      mqttClient.publish(topicPublishMotion1Voltage, text+2); // skip first two characters (V and colon)
+    }
+    
+    if((strncmp(text,"B",1)==0 || strncmp(text,"I",1)==0) && strlen(text)>2) {
+      Serial.println(F("illuminance reported by motion sensor 1"));
+      mqttClient.publish(topicPublishMotion1Illuminance, text+2); // skip first two characters (I and colon)
+    }
+  }
+  
+  // check data on generic nRF24 address
+  while(radio.available(nRF24Addresses[0])) {
     char text[16] = {0};
     radio.read(&text, sizeof(text));
     Serial.print(F("nRF24 payload received: "));
@@ -152,6 +236,23 @@ void loop() {
 
   mqttClient.loop();
   delay(100);
+
+  // send keepalive message to MQTT broker and measure roughly every half hour
+  if(keepAliveCounter >= 18000UL) {
+    keepAliveCounter = 0;
+
+    mqttClient.publish(topicPublishIsAlive, "ping");
+
+    if (!envSensor.run())
+    {
+        checkBsecStatus(envSensor);
+    }
+  }
+
+  if (!envSensor.run())
+  {
+    checkBsecStatus(envSensor);
+  }
 }
  
 // MQTT callback function
@@ -182,3 +283,86 @@ void mqttCallback(const char topic[], byte* payload, unsigned int length) {
   }
 }
 
+
+void newDataCallback(const bme68xData data, const bsecOutputs outputs, Bsec2 bsec)
+{
+    if (!outputs.nOutputs)
+    {
+        return;
+    }
+
+    Serial.println("BSEC outputs:\n\tTime stamp = " + String((int) (outputs.output[0].time_stamp / INT64_C(1000000))));
+    for (uint8_t i = 0; i < outputs.nOutputs; i++)
+    {
+        const bsecData output  = outputs.output[i];
+        switch (output.sensor_id)
+        {
+            case BSEC_OUTPUT_IAQ:
+                
+                break;
+            case BSEC_OUTPUT_STABILIZATION_STATUS:
+                Serial.println("\tStabilization status = " + String(output.signal));
+                break;
+            case BSEC_OUTPUT_RUN_IN_STATUS:
+                Serial.println("\tRun in status = " + String(output.signal));
+                break;
+            case BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE:
+                sprintf(buffer,"%.1f", output.signal);
+                Serial.print("\tCompensated temperature = ");Serial.println(buffer);
+                mqttClient.publish(topicPublishSensorTemperature, buffer);
+                break;
+            case BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY:
+                sprintf(buffer,"%.1f", output.signal);
+                Serial.print("\tCompensated humidity = ");Serial.println(buffer);
+                mqttClient.publish(topicPublishSensorHumidity, buffer);
+                break;
+            case BSEC_OUTPUT_STATIC_IAQ:
+                sprintf(buffer,"%.1f", output.signal);
+                Serial.print("\tIAQ static = ");Serial.println(buffer);
+                mqttClient.publish(topicPublishSensorIaq, buffer);
+
+                sprintf(buffer,"%d", output.accuracy);
+                Serial.print("\tIAQ static accuracy = ");Serial.println(buffer);
+                mqttClient.publish(topicPublishSensorIaqAccuracy, buffer);
+
+                break;
+            case BSEC_OUTPUT_CO2_EQUIVALENT:
+                sprintf(buffer,"%.1f", output.signal);
+                Serial.print("\tCO2 Equivalent = ");Serial.println(buffer);
+                mqttClient.publish(topicPublishSensorCo2, buffer);
+                break;
+            case BSEC_OUTPUT_BREATH_VOC_EQUIVALENT:
+                Serial.println("\tbVOC equivalent = " + String(output.signal));
+                break;
+            case BSEC_OUTPUT_GAS_PERCENTAGE:
+                Serial.println("\tGas percentage = " + String(output.signal));
+                break;
+            case BSEC_OUTPUT_COMPENSATED_GAS:
+                Serial.println("\tCompensated gas = " + String(output.signal));
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void checkBsecStatus(Bsec2 bsec)
+{
+    if (bsec.status < BSEC_OK)
+    {
+        Serial.println("BSEC error code : " + String(bsec.status));
+    }
+    else if (bsec.status > BSEC_OK)
+    {
+        Serial.println("BSEC warning code : " + String(bsec.status));
+    }
+
+    if (bsec.sensor.status < BME68X_OK)
+    {
+        Serial.println("BME68X error code : " + String(bsec.sensor.status));
+    }
+    else if (bsec.sensor.status > BME68X_OK)
+    {
+        Serial.println("BME68X warning code : " + String(bsec.sensor.status));
+    }
+}

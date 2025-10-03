3a3831ca5481490b57e093cf2f6ff43cc7f1776c
hagego <hagen.goller@posteo.de>
2025-10-03 19:26:18 +0200

    Initial version of nRF24  EspRemoteControl

---
+++ nRF24/EspRemoteControl/src/EspRemoteControl.cpp
@@ -0,0 +1,223 @@
+/**
+ * @file EspReceiver.cpp
+ */
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <time.h>
+
+
+#include "WifiInfo.h"
+#include "MqttInfo.h"
+
+
+// speed of serial interface for debug messages
+#define SERIAL_SPEED 74880
+ 
+ // nRF24 CE/CSN pins
+const uint8_t PIN_CE  = 15; // D8 on D1 mini (SPI CS)
+const uint8_t PIN_CSN = 0;  // D3 on D1 mini (GPIO0)
+
+ // local motion sensor pin
+const uint8_t PIN_MOTION_SENSOR = 16; // D0 on D1 mini (GPIO16)
+
+// nRF24 addresses to listen to
+// 0:   debug:   generic
+// 1-5: <i>clnt: clients 1-5
+//uint8_t nRF24Addresses[][6] = {"debug", "1clnt", "2clnt", "3clnt", "4clnt", "5clnt"}; // max. 6 addresses possible
+uint8_t nRF24Addresses[][6] = {"1clnt"};
+
+// nRF24 paload size
+const uint8_t nRF24PayloadSize = 16; // max. 32 bytes possible
+
+ // global WiFi, MQTT, RF24 and Bsecs client objects
+WiFiClient   wifiClient;
+PubSubClient mqttClient(wifiClient);
+RF24         radio(PIN_CE, PIN_CSN);  // create an RF24 object, CE, CSN
+
+ 
+ // global buffer object for sprinf and other string operations
+ char buffer[256];
+
+  // full MQTT client name (client ID + IP address)
+ char mqttFullClientName[128];
+ 
+ // MQTT callback function
+ void mqttCallback(const char topic[], byte* payload, unsigned int length);
+
+ 
+void setup() {
+  // initialize GPIO pins
+  pinMode(PIN_MOTION_SENSOR, INPUT); // local motion sensor pin
+  
+  // initialize serial interface
+  Serial.begin(SERIAL_SPEED);
+  delay(100);
+  Serial.println();
+  Serial.println(F("nRF24 receiver started"));
+ 
+  // Connect to WiFi network
+  Serial.print(F("Connecting to SID "));
+  Serial.println(WIFI_SSID);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PSK);
+ 
+  static int COUNTER_MAX = 100;
+  int counter = 0;
+  while (WiFi.status() != WL_CONNECTED && counter<COUNTER_MAX) {
+    delay(100);
+    Serial.print(".");
+    counter++;
+  }
+
+  if(counter>=COUNTER_MAX) {
+    // timeout reached. sleep and try again
+    Serial.print(F("Connection failed"));
+    ESP.restart();
+  }
+ 
+  Serial.print(F("\nConnected to WiFi, IP address="));
+  Serial.println(WiFi.localIP());
+ 
+  // connect to MQTT broker
+  sprintf(mqttFullClientName,"%s-%s",MQTT_CLIENT_ID,WiFi.localIP().toString().c_str());
+  mqttClient.setServer(MQTT_SERVER,MQTT_PORT);
+  mqttClient.setCallback(mqttCallback);
+
+  sprintf(buffer,"Attempting MQTT connection to broker at %s as client %s",MQTT_SERVER,mqttFullClientName);
+  Serial.println(buffer);
+
+  // Attempt to connect
+  if (!mqttClient.connect(mqttFullClientName)) {
+    Serial.print(F("MQTT connect failed, rc="));
+    Serial.print(mqttClient.state());
+
+    ESP.restart();
+  }
+
+  Serial.println(F("MQTT connected"));
+  // Once connected, publish an announcement...
+  sprintf(buffer,"connected as %s",mqttFullClientName);
+  mqttClient.publish(topicPublishConnected, buffer);
+  mqttClient.publish(topicPublishIsAlive, mqttFullClientName);
+
+  // subscribe to topics
+  mqttClient.subscribe(topicSubscribeSetAddress);
+
+  
+  // start nRF24 radio
+  radio.begin();
+ 
+  // listen to all addresses in nRF24Addresses
+  for(uint8 i=0; i<sizeof(nRF24Addresses)/sizeof(nRF24Addresses[0]); i++) {
+    Serial.printf("Listening to nRF24 address %d: %s\n",i,nRF24Addresses[i]);
+    radio.openReadingPipe(i, nRF24Addresses[i]);
+  }
+
+  radio.setPayloadSize(nRF24PayloadSize);
+  radio.startListening();                   // set module as receiver
+
+}
+ 
+// Main loop
+
+u32_t keepAliveCounter = 0;                // use simple counter to send keepalive messages to MQTT broker roughly every half hour
+u_int8_t oldLocalMotionSensorState = 0;    // used to detect changes in local motion sensor state
+
+void loop() {
+  keepAliveCounter++;
+
+  // reconnect to WIFI if needed
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WIFI disconnected");
+    WiFi.begin(WIFI_SSID, WIFI_PSK);
+    uint8_t timeout = 60;
+    while (timeout && (WiFi.status() != WL_CONNECTED)) {
+      timeout--;
+      delay(1000);
+    }
+    if(WiFi.status() == WL_CONNECTED) {
+      Serial.println("WIFI reconnected");
+
+      sprintf(buffer,"%s-%d",MQTT_CLIENT_ID,ESP.getChipId());
+      if (mqttClient.connect(buffer)) {
+        sprintf(buffer,"reconnected in loop() to MQTT broker at %s as client %s-%d, local IP=%s",MQTT_SERVER,MQTT_CLIENT_ID,ESP.getChipId(),WiFi.localIP().toString().c_str());
+        Serial.println(buffer);
+
+        // Once connected, publish an announcement...
+        mqttClient.publish(topicPublishConnected, mqttFullClientName);
+      }
+    }
+    else {
+      Serial.println("WIFI reconnect failed. Rebooting...");
+      // reboot
+      ESP.restart();
+    }
+  }
+
+  if(!mqttClient.connected()) {
+    Serial.println(F("MQTT connection lost - restarting"));
+    ESP.restart();
+  }
+  
+
+  uint8_t pipe;
+      while(radio.available(&pipe)) {
+
+      char text[16] = {0};
+      radio.read(&text, sizeof(text));
+      sprintf(buffer,"payload received on address %d: %s",pipe,text);
+      Serial.println(buffer);
+      mqttClient.publish(topicPublishPayloadReceived, buffer);
+  }
+
+
+
+
+
+  
+
+  // send keepalive message to MQTT broker and read local sensor roughly every 5 minutes
+  if(keepAliveCounter >= 30000UL) {
+    keepAliveCounter = 0;
+
+    Serial.println(F("Sending ping"));
+    mqttClient.publish(topicPublishIsAlive, mqttFullClientName);
+  }
+
+  mqttClient.loop();
+  delay(10);
+}
+ 
+// MQTT callback function
+void mqttCallback(const char topic[], byte* payload, unsigned int length) {
+  // ignore zero length payloads to avoid endless loop
+  if(length==0) {
+    Serial.println(F("MQTT callback: zero length payload - ignoring"));
+    return;
+  }
+
+  char topicBuffer[strlen(topic)+1];
+  strcpy(topicBuffer,topic);
+
+  char payloadString[length+1];
+  strncpy(payloadString,(char*)payload,length);
+  payloadString[length] = 0;
+
+  sprintf(buffer,"MQTT message arrived [%s] value=%s",topic,payloadString);
+  Serial.println(buffer);
+
+  if(strcmp(topic,topicSubscribeSetAddress)==0) {
+    if(length>5) {
+      Serial.println(F("MQTT callback: nRF24 address too long - ignoring"));
+      return;
+    }
+
+    radio.openReadingPipe(0,(const uint8_t*)payloadString); // set the new nRF24 address
+  }
+}
+
